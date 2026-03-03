5bb761b6da6ba7bf0904964f910d69a94b72bf95
HugoMaster <hcalderonhuerta@gmail.com>
2026-03-03 11:44:33 -0600

    ya se subieron los archivos

---
+++ JS_1.js
@@ -0,0 +1,10 @@
+const array1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+console.log(" " + array1)
+var array2 = []
+var i=0
+while (i<=20){
+    array2.push(array1[i])
+    i++
+}
+console.log("Array 1 :"+array1)
+console.log("Array 2 :"+array2)
